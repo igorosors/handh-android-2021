4d34f2a15c69b26dfa39a7e37b2b8daa243d7eb8
Igor <89833375+igorosors@users.noreply.github.com>
2021-11-17 19:35:42 +0300

    Delete Lesson_4_Strelyukhin directory

--- Lesson_4_Strelyukhin/app/src/main/java/com/example/lesson_4_strelyukhin/AdapterElement.kt
+++
@@ -1,4 +0,0 @@
-package com.example.lesson_4_strelyukhin
-
-interface AdapterElement {
-}
--- Lesson_4_Strelyukhin/app/src/main/java/com/example/lesson_4_strelyukhin/model/Base.kt
+++
@@ -1,8 +0,0 @@
-package com.example.lesson_4_strelyukhin.model
-
-import com.example.lesson_4_strelyukhin.AdapterElement
-
-data class Base (
-    val title: String,
-    val icon: Int,
-        ) : AdapterElement
